dddabd6d5842e561a85ffc459dc4f4ad388c045b
Weslley Ferraz <weslleyhenrique800@gmail.com>
2023-01-02 06:56:13 -0400

    qa

--- src/sever.ts
+++ src/sever.ts
@@ -1,6 +1,19 @@
 import express from 'express';
+import { createServer } from 'http';
+import { Server } from 'socket.io';
+import path from 'path';
 
 const app = express();
+
+app.use(express.static(path.join(__dirname, '..', 'public')));
+
+const server = createServer(app);
+
+const io = new Server(server);
+
+io.on('connection', (socket) => {
+  console.log('Socket', socket);
+});
 
 const port = 5000;
 
@@ -8,4 +21,4 @@ app.get('/', (request, response) => {
   return response.json({ message: 'Olá Mundo' });
 });
 
-app.listen(port, () => console.log(`Sever is running on port: ${port}`));
+server.listen(port, () => console.log(`Sever is running on port: ${port}`));
